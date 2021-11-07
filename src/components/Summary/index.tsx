65a1a2369a5e7392d1f6434de1c1ddae66bea1a8
Jonatas Weverton <jonataswevertom@hotmail.com>
2021-11-07 01:07:55 -0300

    #6 commit

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,5 +1,32 @@
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { Container } from "./styles";
+
 export function Summary() {
     return (
-        <h1>Summary</h1>
+        <Container >
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="Entradas" />
+                </header>
+                <strong className="deposit">+ R$17.400,00</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImg} alt="Saidas" />
+                </header>
+                <strong className="withdraw">- R$1.259,00</strong>
+            </div>
+            <div className="highligh-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>= R$16.141,00</strong>
+            </div>
+        </Container>
     )
 }
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./style";
+
+export function TransactionsTable() {
+  useEffect(() => {
+    api.get('transactions')
+    .then(response => console.log(response.data))
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Preço</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td className="title">Desenvolvimento de site</td>
+            <td className="deposit">+ R$12.000,00</td>
+            <td className="category">Venda</td>
+            <td className="data">13/04/2021</td>
+          </tr>
+          <tr>
+            <td className="title">Aluguel do apartamento</td>
+            <td className="withdraw">- R$1.200,00</td>
+            <td className="category">Casa</td>
+            <td className="data">27/03/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
